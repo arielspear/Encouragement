d0505145f986f6b2d3cd409dda95bf7b318bdbc6
Ariel Spear <arielspear@github.com>
2015-11-09 16:55:41 -0800

    end of day

--- app/src/main/java/com/arielspear/encouragement/MainActivity.java
+++ app/src/main/java/com/arielspear/encouragement/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declare View variables and assign Views from layout files
+        TextView quoteLabel = (TextView) findViewById(R.id.quoteTextView);
+        Button showQuoteButton = (Button) findViewById(R.id.showQuoteButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+
+        showQuoteButton.setOnClickListener(listener);
     }
 
     @Override
